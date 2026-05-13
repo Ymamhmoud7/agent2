0b5099ddb5a3fe833aec0acca3dbafe6d0356d38
RealYosuefMA <mt2945brpr@privaterelay.appleid.com>
2026-05-13 23:57:07 +0300

    Reading Python Skills

--- utils/read_skill.py
+++ utils/read_skill.py
@@ -1,3 +1,14 @@
 def read_skill(skill_name):
     with open(f'skills/{skill_name}.py', 'r') as f:
-        return f.read()
+        file = f.read()
+        functions = []
+        for line in file.splitlines():
+            if line.startswith('def '):
+                func_name = line.split('def ')[1].split('(')[0]
+                functions.append(func_name)
+        
+        dict = {
+            'name': skill_name,
+            'functions': functions
+        }
+    return dict
